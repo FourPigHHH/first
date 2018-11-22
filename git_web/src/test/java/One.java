20caff84a0ca22ee69bc3740de19e4a6e28d2d0e
OneMoron <cyy970527@aliyun.com>
2018-11-22 21:15:05 +0800

    1

--- git_web/src/test/java/One.java
+++ git_web/src/test/java/One.java
@@ -1,11 +1,7 @@
 public class One {
     public static void main(String[] args) {
         System.out.println("one one one");
-<<<<<<< HEAD
  System.out.println("........");
-=======
- System.out.println("two two two");
->>>>>>> 2f4dc827189e754d4f5f90cc979a39b88532db5c
  System.out.println("xxxxxxxx");
  System.out.println("xxxxxxxx");
  System.out.println("xiaona");
