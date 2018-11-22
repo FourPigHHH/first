1a482db933837e6ed537070b70a7a4ae3c73009d
xutianzuo <2235554567@qq.com>
2018-11-22 21:13:00 +0800

    徐天佐的修改

--- git_web/src/test/java/One.java
+++ git_web/src/test/java/One.java
@@ -1,6 +1,7 @@
 public class One {
     public static void main(String[] args) {
         System.out.println("one one one");
- System.out.println("one one one");
+		System.out.println("one one one222");
+		System.out.println("one one one");
     }
 }
